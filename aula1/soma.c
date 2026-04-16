3391034d3f903b1b2dd868525f71fc04bda15835
kassiods <sapokk.pinga@gmail.com>
2026-04-16 14:47:19 -0300

    First commit

---
+++ aula1/soma.c
@@ -0,0 +1,15 @@
+//soma de variaveis
+
+#include <stdio.h>
+int main (){
+    int a;
+    int b, c;
+
+    a=7+9;
+    b=a+10;
+    c=b-a;
+
+    printf("O valor de a é = %d\n", a);
+    printf("O valor de b é = %d\n", b);
+    printf("O valor de c é = %d\n", c);
+}
